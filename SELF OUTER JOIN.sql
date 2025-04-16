c41ac2d3f2663e7cf093a916be6198105bec01d5
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-16 14:28:05 +0530

    self outer join

--- SELF OUTER JOIN.sql
+++ SELF OUTER JOIN.sql
@@ -3,4 +3,9 @@ SELF OUTER JOIN
 
 use sql_hr;
 
-SELECT e.employee_id , e.first_name , e.job_title , m.first_name AS Manager FROM employees e LEFT JOIN employees m  on e.reports_to = m.employee_id;
+
+EXPLAIN ANALYZE SELECT e.employee_id , e.first_name , e.job_title ,
+ m.first_name 
+ AS Manager FROM employees e 
+ LEFT JOIN employees m  on e.reports_to = m.employee_id;
+
