2a58dec57eb6ba1fad2d712b6b40aff0e0ddcacb
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-17 23:22:59 +0530

    insert

--- Insert.sql
+++ Insert.sql
@@ -21,3 +21,29 @@ VALUES (
    );
 
    
+
+
+INSERT INTO shippers (name) VALUES (
+    'Globelink Weststar Shipping , Dubai'
+)
+
+-- Inserting multiple rows/items into the row
+
+    INSERT INTO shippers (name) VALUES (
+        'Globelink Weststar Shipping , India'
+    ),
+    (    'Lavinstar Logistics Pvt Ltd , India'),
+    (    "Vinworld America , USA");
+
+
+
+-- inserting into multiple tables
+ INSERT INTO orders (customer_id , order_date , status) VALUES
+ (2 , '2019-01-02' , 1);
+
+ SELECT LAST_INSERT_ID();
+
+ INSERT INTO order_items 
+ (order_id ,product_id , quantity , unit_price)
+  VALUES 
+ (LAST_INSERT_ID() , 1 ,  4  , 2.25);
