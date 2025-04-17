6c54233e43c00ce3eb26cddbf3e105464e60c0ad
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-17 11:17:54 +0530

    union

---
+++ Insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO customers 
+(
+    first_name,
+    last_name,
+    birth_date,
+    phone,
+    address,
+    city,
+    state,
+    points
+)
+VALUES ( 
+"Ashish" , 
+"Srivastava" ,
+ '1996-01-22' ,
+'7007823127',
+'Indira Nagar',
+'Lucknow',
+'UP',
+100
+   );
+
+   
--- Unions.sql
+++ Unions.sql
@@ -1,5 +1,24 @@
+-- UNION
+
+
+
 SELECT order_id , order_date , "ACTIVE" AS Status 
 FROM orders WHERE order_date >= '2019-01-01'
 UNION
 SELECT order_id , order_date , "ARCHIVED" AS Status 
 FROM orders WHERE order_date <= '2019-01-01';
+
+
+
+-- Exercise
+
+SELECT customer_id , first_name , points ,  "Bronze" AS Type 
+FROM  customers WHERE points <= 2000
+UNION
+SELECT customer_id , first_name , points ,  "Silver" AS Type 
+FROM  customers WHERE points BETWEEN 2001 AND 3000 
+UNION
+SELECT customer_id , first_name , points ,  "Gold" AS Type 
+FROM  customers WHERE points > 3000  ORDER BY first_name;
+
+
