6cbf4d6386414ad3a8cc21455f8da8f4ca237541
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-19 23:46:58 +0530

    left , self join question

---
+++ Advance/SubQueries.sql
@@ -0,0 +1,30 @@
+-- Find products that are more expensive than Lettuce (id = 3)
+
+use sql_store;
+
+
+SELECT * FROM products WHERE unit_price > (
+    SELECT unit_price FROM products WHERE product_id = 3
+);
+
+
+
+SELECT * FROM products WHERE unit_price >  3
+;
+
+
+
+
+use sql_hr;
+
+
+-- was checking self join question
+SELECT e.employee_id , e.first_name , m.first_name FROM 
+employees e  LEFT JOIN  employees m
+ON e.reports_to = m.employee_id;
+
+
+
+-- Excersise
+-- In sql_hr database : 
+-- Find the employees who earn more than average
