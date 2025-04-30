3dbf29fb8e99bdf1b44ae907b7716bd2b51e0907
ashbbdu <ashishsrivastava7007@gmail.com>
2025-04-30 17:50:09 +0530

    few queries

--- dashboard/ShipmentProfile.sql
+++ dashboard/ShipmentProfile.sql
@@ -20,3 +20,18 @@ SELECT * FROM shipment_profile LIMIT 10;
 SELECT Local_Client_Name , SUM(
 
 )  FROM
+
+
+
+
+( SELECT * 
+    FROM shipment_profile 
+   WHERE Company = 'LSL' 
+   LIMIT 10
+)
+UNION ALL                
+( SELECT * 
+    FROM shipment_profile 
+   WHERE Company = 'INJ' 
+   LIMIT 10
+);
