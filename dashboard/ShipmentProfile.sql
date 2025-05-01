4a553c81304e8720c3ec02ae8e7845bf03041225
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-01 22:57:35 +0530

    few queries

--- dashboard/ShipmentProfile.sql
+++ dashboard/ShipmentProfile.sql
@@ -22,15 +22,3 @@ SELECT Local_Client_Name , SUM(
 )  FROM
 
 
-
-( SELECT * 
-    FROM shipment_profile 
-   WHERE Company = 'LSL' 
-   LIMIT 10
-)
-UNION ALL                
-( SELECT * 
-    FROM shipment_profile 
-   WHERE Company = 'INJ' 
-   LIMIT 10
-);
