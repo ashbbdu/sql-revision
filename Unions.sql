1ef0c3dde0004ba9ca289451005da7b0ec08dd2e
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-07 22:43:33 +0530

    chnages

--- Unions.sql
+++ Unions.sql
@@ -33,6 +33,15 @@ UNION
 SELECT customer_id , first_name , points ,  "Gold" AS Type 
 FROM  customers WHERE points > 3000  ORDER BY first_name;
 
+SELECT customer_id , first_name , points ,  "Bronze" AS Type 
+FROM  customers WHERE points <= 2000
+UNION
+SELECT customer_id , first_name , points ,  "Silver" AS Type 
+FROM  customers WHERE points BETWEEN 2001 AND 3000 
+UNION
+SELECT customer_id , first_name , points ,  "Gold" AS Type 
+FROM  customers WHERE points > 3000  ORDER BY first_name;
+
 
 
 
