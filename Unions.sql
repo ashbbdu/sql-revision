d4fb48d150cc0bf122f56df9d57882960f7bc4c8
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-10 23:17:10 +0530

    changes

--- Unions.sql
+++ Unions.sql
@@ -25,20 +25,6 @@ FROM  customers WHERE points > 3000  ORDER BY first_name;
 
 
 
--- Exercise
-
-use sql_store;
-
-SELECT customer_id , first_name , points ,  "Bronze" AS Type 
-FROM  customers WHERE points <= 2000
-UNION
-SELECT customer_id , first_name , points ,  "Silver" AS Type 
-FROM  customers WHERE points BETWEEN 2001 AND 3000 
-UNION
-SELECT customer_id , first_name , points ,  "Gold" AS Type 
-FROM  customers WHERE points > 3000  ORDER BY first_name;
-
-
 
 
 
