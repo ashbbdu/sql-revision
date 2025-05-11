bc84a6356421aac82a2d1d0478e5dc0dfbfbda6b
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-11 23:44:52 +0530

    changes

--- Unions.sql
+++ Unions.sql
@@ -14,6 +14,7 @@ FROM orders WHERE order_date <= '2019-01-01';
 
 use sql_store;
 
+
 SELECT customer_id , first_name , points ,  "Bronze" AS Type 
 FROM  customers WHERE points <= 2000
 UNION
@@ -23,6 +24,17 @@ UNION
 SELECT customer_id , first_name , points ,  "Gold" AS Type 
 FROM  customers WHERE points > 3000  ORDER BY first_name;
 
+
+
+
+SELECT customer_id , first_name , points ,  "Bronze" AS Type 
+FROM  customers WHERE points <= 2000
+UNION
+SELECT customer_id , first_name , points ,  "Silver" AS Type 
+FROM  customers WHERE points BETWEEN 2001 AND 3000 
+UNION
+SELECT customer_id , first_name , points ,  "Gold" AS Type 
+FROM  customers WHERE points > 3000  ORDER BY first_name;
 
 
 
